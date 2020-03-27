4a3f9a35b66ec648a5ef99c296a9cbee4fe154f8
Nikhil <you@example.com>
2020-03-27 13:37:05 +0530

    Added: Bet win or loose

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -4,3 +4,20 @@ printf "Welcome to Gambling Simulation \n"
 
 STAKE=100
 BET=1
+BetAmount=$STAKE
+
+function betting()
+{
+	Number=$((RANDOM%2))
+
+	if [[ $Number -eq 1 ]]
+	then
+		echo "you won 1 bet"
+		((BetAmount++))
+	else
+		echo "you loose 1 bet"
+		((BetAmount--))
+	fi
+	echo $BetAmount
+}
+betting
